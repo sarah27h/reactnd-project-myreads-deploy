8394d68293da96b05ee823755084c2f1c00bf6a7
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-08-12 20:32:14 +0200

    Refactor: added the basename prop to my Router
     (aliased from BrowserRouter)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+ReactDOM.render(
+<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root')
+);
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+// ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
